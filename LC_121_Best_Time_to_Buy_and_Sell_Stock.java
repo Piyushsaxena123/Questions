8539df0562fa27cf6677dd472639037021a01368
Piyushsaxena 123 <piyushsaxena7723@gmail.com>
2025-11-08 23:58:20 +0530

    Solved: LC 121 Best Time to Buy and Sell Stock | Topic: Array | Greedy

--- LC_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ LC_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,2 +1,20 @@
-// 121 leetcode - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
-// Approach: your short explanation (1–4 lines)
+// LeetCode Question: Best Time to Buy and Sell Stock
+// Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+// Approach: Keep track of minimum price so far and maximum profit margin.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+        for(int p : prices){
+            min = Math.min(min, p);
+            profit = Math.max(profit, p - min);
+        }
+        return profit;
+    }
+}
+
+
+
